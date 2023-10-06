191c0b7c205b0755df011fa2ca806e5cd481f4f6
KlausStille <mail@klausstille.com>
2023-10-06 15:26:38 +0200

    revised folder structure

--- sessions/recap-project-6/REVISITED/tourio-app/pages/api/places/index.js
+++
@@ -1,5 +0,0 @@
-import { db_places } from '../../../lib/db_places';
-
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
-}
--- sessions/recap-project-6/tourio-app/pages/places/[id]/index.js
+++ sessions/recap-project-6/tourio-app/pages/places/[id]/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { StyledLink } from '../../../components/StyledLink.js';
 import { StyledButton } from '../../../components/StyledButton.js';
 import { StyledImage } from '../../../components/StyledImage.js';
+import Comments from '../../../components/Comments.js';
 
 const ImageContainer = styled.div`
   position: relative;
@@ -32,8 +33,11 @@ export default function DetailsPage() {
   const router = useRouter();
   const { isReady } = router;
   const { id } = router.query;
-
-  const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
+  const {
+    data: { place, comments } = {},
+    isLoading,
+    error,
+  } = useSWR(`/api/places/${id}`);
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
 
@@ -72,6 +76,7 @@ export default function DetailsPage() {
           Delete
         </StyledButton>
       </ButtonContainer>
+      <Comments locationName={place.name} comments={comments} />
     </>
   );
 }
