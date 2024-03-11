ee829b04caab3af87e542d238f2e90a55fb00077
KlausStille <mail@klausstille.com>
2024-03-11 16:46:57 +0100

    added one more challenge to destructuring-objects challenge

--- sessions/js-modern-syntax/destructuring-objects/index.js
+++ sessions/js-modern-syntax/destructuring-objects/index.js
@@ -2,7 +2,7 @@ console.clear();
 
 // EXERCISE 1
 // Extract only the course name from the company object.
-// Make sure to export your variable as in the example below to make the tests work.
+// Make sure to export your variable/function as in the example below to make the tests work.
 
 const company = {
   name: "Spiced Academy",
@@ -40,3 +40,26 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+
+// EXERCISE 5
+// Rewrite the following function (logInfo) to use destructuring assignment for the three variables it creates:
+
+const logInfo = (city) => {
+  const name = city.name;
+  const country = city.country;
+  const numPeople = city.population;
+
+  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`
+}
+
+const destructuringLogInfo = (city) => {
+  // (Hint: Lines 48-50 should be replaced with a single line and you shouldn't change anything else).
+  // your code here
+
+
+  // your code here
+  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`
+}
+
+// This is how you would call it:
+ destructuringLogInfo({ name: "Marseille", country: "France", population: 861635 });
